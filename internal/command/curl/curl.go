ad4d1ccd28a0a870524ef91905494fefdb900ef9
Joshua Sierles <joshua@fly.io>
2022-11-22 21:42:26 +0100

    Replace fly curl with Planetfall API

--- internal/command/curl/curl.go
+++ internal/command/curl/curl.go
@@ -8,22 +8,18 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/url"
-	"sort"
+	"os"
 	"strconv"
-	"sync"
-	"time"
 
-	"github.com/dustin/go-humanize"
 	"github.com/spf13/cobra"
 
 	"github.com/superfly/flyctl/api"
-	"github.com/superfly/flyctl/client"
 	"github.com/superfly/flyctl/internal/command"
 	"github.com/superfly/flyctl/internal/config"
 	"github.com/superfly/flyctl/internal/flag"
+	"github.com/superfly/flyctl/internal/logger"
 	"github.com/superfly/flyctl/internal/render"
 	"github.com/superfly/flyctl/iostreams"
 )
@@ -44,113 +40,71 @@ func New() (cmd *cobra.Command) {
 	return
 }
 
+var Regions []string = []string{
+	"aws:af-south-1",
+	"aws:ap-east-1",
+	"aws:ap-northeast-1",
+	"aws:ap-northeast-2",
+	"aws:ap-northeast-3",
+	"aws:ap-south-1",
+	"aws:ap-southeast-1",
+	"aws:ap-southeast-2",
+	"aws:ap-southeast-3",
+	"aws:ca-central-1",
+	"aws:eu-central-1",
+	"aws:eu-north-1",
+	"aws:eu-south-1",
+	"aws:eu-west-1",
+	"aws:eu-west-2",
+	"aws:eu-west-3",
+	"aws:me-south-1",
+	"aws:sa-east-1",
+	"aws:us-east-1",
+	"aws:us-east-2",
+	"aws:us-west-1",
+	"aws:us-west-2",
+	"fly:ams",
+	"fly:fra",
+}
+
 func run(ctx context.Context) error {
 	url, err := url.Parse(flag.FirstArg(ctx))
 	if err != nil {
 		return fmt.Errorf("invalid URL specified: %w", err)
 	}
 
-	regionCodes, err := fetchRegionCodes(ctx)
-	if err != nil {
-		return err
-	}
+	// regionCodes, err := fetchRegionCodes(ctx)
+	// if err != nil {
+	// 	return err
+	// }
 
-	rws, err := prepareRequestWrappers(ctx, url, regionCodes)
+	response, err := fetchTimings(ctx, url)
 	if err != nil {
-		return err
-	}
-
-	timings := gatherTimings(ctx, rws)
-	if err := ctx.Err(); err != nil {
 		return err
 	}
 
 	if io := iostreams.FromContext(ctx); !config.FromContext(ctx).JSONOutput {
-		renderTextTimings(io.Out, io.ColorScheme(), timings)
-	} else {
-		renderJSONTimings(io.Out, timings)
+		renderTextTimings(io.Out, io.ColorScheme(), response.Data.Regions)
 	}
 
+	fmt.Printf("%s\n", response.Data.Link)
+	fmt.Println()
 	return nil
 }
 
-func fetchRegionCodes(ctx context.Context) (codes []string, err error) {
-	client := client.FromContext(ctx).API()
-
-	var regions []api.Region
-	if regions, _, err = client.PlatformRegions(ctx); err != nil {
-		err = fmt.Errorf("failed retrieving regions: %w", err)
-
-		return
-	} else if len(regions) == 0 {
-		err = errors.New("no regions could be retrieved")
-
-		return
-	}
-
-	for _, region := range regions {
-		codes = append(codes, region.Code)
-	}
-	sort.Strings(codes)
-
-	return
+type PlanetfallPayload struct {
+	Method  string   `json:"method"`
+	Url     string   `json:"url"`
+	Regions []string `json:"regionIds"`
+	Repeat  bool     `json:"repeat"`
 }
 
-func prepareRequestWrappers(ctx context.Context, url *url.URL, regionCodes []string) (rws []*requestWrapper, err error) {
-	for _, region := range regionCodes {
-		var rw *requestWrapper
-		if rw, err = wrapRequestForRegion(ctx, region, url); err != nil {
-			err = fmt.Errorf("failed preparing request for %s: %w", region, err)
+func fetchTimings(ctx context.Context, url *url.URL) (resp *Response, err error) {
 
-			break
-		}
-
-		rws = append(rws, rw)
-	}
-
-	return
-}
-
-func gatherTimings(ctx context.Context, rws []*requestWrapper) (timings []*timing) {
-	var wg sync.WaitGroup
-	wg.Add(len(rws))
-
-	c := make(chan *timing, len(rws))
-
-	for i := range rws {
-		go func(rw *requestWrapper) {
-			defer wg.Done()
-
-			rw.time(c)
-		}(rws[i])
-	}
-
-	wg.Wait()
-	close(c)
-
-	for t := range c {
-		timings = append(timings, t)
-	}
-
-	sort.Slice(timings, func(i, j int) bool {
-		return timings[i].region < timings[j].region
-	})
-
-	return
-}
-
-type requestWrapper struct {
-	request    *http.Request
-	regionCode string
-}
-
-func wrapRequestForRegion(ctx context.Context, regionCode string, url *url.URL) (rw *requestWrapper, err error) {
-	payload := struct {
-		URL    string `json:"url"`
-		Region string `json:"region"`
-	}{
-		URL:    url.String(),
-		Region: regionCode,
+	payload := PlanetfallPayload{
+		Url:     url.String(),
+		Regions: Regions,
+		Method:  "GET",
 	}
 
 	var buf bytes.Buffer
@@ -159,104 +113,113 @@ func wrapRequestForRegion(ctx context.Context, regionCode string, url *url.URL) 
 	}
 
 	var r *http.Request
-	if r, err = http.NewRequestWithContext(ctx, http.MethodPost, "https://curl.fly.dev/timings", &buf); err != nil {
+	if r, err = http.NewRequestWithContext(ctx, http.MethodPost, "https://api.planetfall.io/v1/fly/curl", &buf); err != nil {
 		return
 	}
+	token := os.Getenv("PLANETFALL_TOKEN")
 
-	r.Header.Add("Authorization", "1q2w3e4r")
+	if token == "" {
+		return nil, errors.New("Please set PLANETFALL_TOKEN")
+	}
+	r.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
 	r.Header.Add("Content-Type", "application/json")
 
-	rw = &requestWrapper{
-		request:    r,
-		regionCode: regionCode,
-	}
+	httpClient, err := api.NewHTTPClient(logger.FromContext(ctx), http.DefaultTransport)
 
-	return
-}
-
-var httpClient = &http.Client{
-	Timeout: time.Second * 3,
-}
-
-func (rw *requestWrapper) time(c chan<- *timing) {
-	t := &timing{
-		region: rw.regionCode,
-	}
-	defer func() {
-		c <- t
-	}()
-
-	res, err := httpClient.Do(rw.request)
 	if err != nil {
-		t.error = err
-
 		return
 	}
+
+	res, err := httpClient.Do(r)
+
+	if err != nil {
+		return
+	}
+
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		if body, err := ioutil.ReadAll(res.Body); err == nil {
-			t.error = errors.New(string(body))
-		} else {
-			t.error = err
+		if body, err := io.ReadAll(res.Body); err == nil {
+			err = errors.New(string(body))
+			return nil, err
 		}
 
 		return
 	}
 
-	if err := json.NewDecoder(res.Body).Decode(t); err != nil {
-		t.error = fmt.Errorf("failed decoding response for %s: %w", rw.regionCode, err)
+	resp = &Response{}
+
+	if err := json.NewDecoder(res.Body).Decode(resp); err != nil {
+		err = fmt.Errorf("failed decoding response: %w", err)
+		return nil, err
 	}
+
+	return
 }
 
-type timing struct {
-	error
-	region string
-
-	HTTPCode          int     `json:"http_code"`
-	SpeedDownload     int     `json:"speed_download"`
-	TimeTotal         float64 `json:"time_total"`
-	TimeNameLookup    float64 `json:"time_namelookup"`
-	TimeConnect       float64 `json:"time_connect"`
-	TimePreTransfer   float64 `json:"time_pretransfer"`
-	TimeAppConnect    float64 `json:"time_appconnect"`
-	TimeStartTransfer float64 `json:"time_starttransfer"`
-	HTTPVersion       string  `json:"http_version"`
-	RemoteIP          string  `json:"remote_ip"`
-	Scheme            string  `json:"scheme"`
+type Response struct {
+	Data Data `json:"data"`
+}
+type Data struct {
+	Link    string   `json:"link"`
+	Regions []Region `json:"regions"`
 }
 
-func (t *timing) formatedHTTPCode(cs *iostreams.ColorScheme) string {
-	text := strconv.Itoa(t.HTTPCode)
-	return colorize(cs, text, float64(t.HTTPCode), 299, 399)
+type Region struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	Checks []Check `json:"checks"`
 }
 
-func (t *timing) formattedDNS() string {
-	return humanize.FtoaWithDigits(t.TimeNameLookup*1000, 1) + "ms"
+type Check struct {
+	ID      string `json:"id"`
+	Latency int    `json:"latency"`
+	Time    int    `json:"time"`
+	Status  int    `json:"status"`
+	Body    string `json:"body"`
+	//headers: Record<string, string>, // just key values
+	Timing Timing `json:"timing"`
+}
+type Timing struct {
+	DnsStart          int `json:"dnsStart"`
+	DnsDone           int `json:"dnsDone"`
+	ConnectStart      int `json:"connectStart"`
+	ConnectDone       int `json:"connectDone"`
+	FirstByteStart    int `json:"firstByteStart"`
+	FirstByteDone     int `json:"firstByteDone"`
+	TlsHandshakeStart int `json:"tlsHandshakeStart"`
+	TlsHandshakeDone  int `json:"tlsHandshakeDone"`
+	TransferStart     int `json:"transferStart"`
+	TransferDone      int `json:"transferDone"`
 }
 
-func (t *timing) formattedConnect(cs *iostreams.ColorScheme) string {
-	timing := t.TimeConnect * 1000
-	text := humanize.FtoaWithDigits(timing, 1) + "ms"
-	return colorize(cs, text, timing, 200, 500)
+func (c *Check) formatedHTTPCode(cs *iostreams.ColorScheme) string {
+	text := strconv.Itoa(c.Status)
+	return colorize(cs, text, c.Status, 299, 399)
 }
 
-func (t *timing) formattedTLS() string {
-	return humanize.FtoaWithDigits((t.TimeAppConnect+t.TimePreTransfer)*1000, 1) + "ms"
+func (t *Timing) formattedDNS() string {
+	dnsTiming := strconv.Itoa(t.DnsDone - t.DnsStart)
+	return dnsTiming + "ms"
 }
 
-func (t *timing) formattedTTFB(cs *iostreams.ColorScheme) string {
-	timing := t.TimeStartTransfer * 1000
-	text := humanize.FtoaWithDigits(timing, 1) + "ms"
+func (t *Timing) formattedConnect(cs *iostreams.ColorScheme) string {
+	connectTiming := t.DnsDone - t.DnsStart
+	text := strconv.Itoa(connectTiming) + "ms"
+	return colorize(cs, text, connectTiming, 200, 500)
+}
+
+func (t *Timing) formattedTLS() string {
+	return strconv.Itoa(t.TlsHandshakeDone-t.TlsHandshakeStart) + "ms"
+}
+
+func (t *Timing) formattedTTFB(cs *iostreams.ColorScheme) string {
+	timing := t.FirstByteStart - t.DnsStart
+	text := strconv.Itoa(timing) + "ms"
 	return colorize(cs, text, timing, 400, 1000)
 }
 
-func (t *timing) formattedTotal() string {
-	timing := t.TimeTotal * 1000
-	return humanize.FtoaWithDigits(timing, 1) + "ms"
-}
-
-func colorize(cs *iostreams.ColorScheme, text string, val, greenCutoff, yellowCutoff float64) string {
+func colorize(cs *iostreams.ColorScheme, text string, val, greenCutoff, yellowCutoff int) string {
 	var fn func(string) string
 	switch {
 	case val <= greenCutoff:
@@ -270,58 +233,22 @@ func colorize(cs *iostreams.ColorScheme, text string, val, greenCutoff, yellowCu
 	return fn(text)
 }
 
-func renderTextTimings(w io.Writer, cs *iostreams.ColorScheme, timings []*timing) {
+func renderTextTimings(w io.Writer, cs *iostreams.ColorScheme, regions []Region) {
 	var rows [][]string
-	for _, t := range timings {
-		if t.error != nil {
-			continue
-		}
+	for _, region := range regions {
+
+		coldCheck := region.Checks[0]
 
 		rows = append(rows, []string{
-			t.region,
-			t.formatedHTTPCode(cs),
-			t.formattedDNS(),
-			t.formattedConnect(cs),
-			t.formattedTLS(),
-			t.formattedTTFB(cs),
-			t.formattedTotal(),
+			region.Name,
+			coldCheck.formatedHTTPCode(cs),
+			coldCheck.Timing.formattedDNS(),
+			coldCheck.Timing.formattedConnect(cs),
+			coldCheck.Timing.formattedTLS(),
+			coldCheck.Timing.formattedTTFB(cs),
+			strconv.Itoa(coldCheck.Latency) + "ms",
 		})
 	}
 
 	render.Table(w, "", rows, "Region", "Status", "DNS", "Connect", "TLS", "TTFB", "Total")
-
-	rows = rows[:0]
-	for _, t := range timings {
-		if t.error == nil {
-			continue
-		}
-
-		rows = append(rows, []string{
-			t.region,
-			t.Error(),
-		})
-	}
-
-	if len(rows) == 0 {
-		return
-	}
-
-	render.Table(w, "Failures", rows, "Region", "Error")
-}
-
-func renderJSONTimings(w io.Writer, timings []*timing) {
-	items := make(map[string]interface{}, len(timings))
-	for _, t := range timings {
-		if t.error != nil {
-			items[t.region] = struct {
-				Error string `json:"error"`
-			}{
-				Error: t.error.Error(),
-			}
-		} else {
-			items[t.region] = t
-		}
-	}
-
-	render.JSON(w, items)
 }
